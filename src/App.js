42dd16a47405561b90a884d4969e887ec0a3659c
Lukas <yangmingdada@gmail.com>
2021-06-12 11:51:36 +0800

    更新antd高级配置

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React,{Component} from "react";
 import {Button} from "antd";
+import './App.less'
 
 
 export default class App extends Component{
  render() {
    return(
-       <div>Hello</div>
+       <div>Hello
+       <Button type="primary">点我</Button>
+       </div>
    )
  }
 }
